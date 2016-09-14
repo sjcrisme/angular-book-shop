28a46c890221d808ef596c7632a7326287ac08ef
Serhii.Verbytskii <sjcris.me@gmail.com>
2016-09-14 15:30:00 +0300

    add index page

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('MyApp', [
+    'ngMaterial',
+ ]).config(function($mdThemingProvider) {
+   $mdThemingProvider
+            .theme('default')
+            .primaryPalette('blue-grey', {
+                'default': '900',
+                'hue-2': '500'
+            })
+            .backgroundPalette('blue-grey', {
+                'default': '200'
+            })
+            .accentPalette('deep-purple');
+});
