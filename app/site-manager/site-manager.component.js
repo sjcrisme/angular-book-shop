fd28ce3f08dd460ca3c0387ea88a98e1554af17a
Serhii.Verbytskii <sjcris.me@gmail.com>
2016-09-15 13:17:39 +0300

    add toast

--- app/site-manager/site-manager.component.js
+++ app/site-manager/site-manager.component.js
@@ -1,11 +1,39 @@
 'use strict';
 
-  function sideNavController($mdSidenav){
+  function sideNavController($mdSidenav,$mdToast){
     var vm = this;
+
+    vm.priceInCart = 100;
 
     vm.openLeftMenu = function() {
         $mdSidenav('left').toggle();
+         vm.priceInCart--;
     }
+    vm.testButton= function() {
+        vm.priceInCart++;
+    }
+
+    //
+    vm.showActionToast = function() {
+
+      var toast = $mdToast.simple()
+        .textContent('You just add book "Drupal How to"')
+        .action('UNDO')
+        .highlightAction(true)
+        .highlightClass('md-accent')// Accent is used by default, this just demonstrates the usage.
+        .parent(document.querySelectorAll('#toaster'))
+        .position('top right');
+
+        $mdToast.show(toast).then(function(response) {
+          if ( response == 'ok' ) {
+            console.log('You clicked the \'UNDO\' action.');
+          }
+        });
+    };
+
+    vm.closeToast = function() {
+      $mdToast.hide();
+    };
   }
     
 angular.module('siteManager',[])
