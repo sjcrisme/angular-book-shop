b0ca9ed2be6a4df842a2938574e63a6d3351c1c1
Serhii.Verbytskii <sjcris.me@gmail.com>
2016-09-28 17:17:48 +0300

    add checkout dialog

--- app/site-manager/site-manager.component.js
+++ app/site-manager/site-manager.component.js
@@ -1,6 +1,6 @@
 'use strict';
 
-  function sideNavController($mdSidenav,ShopCart){
+  function sideNavController($mdSidenav,$mdDialog,ShopCart){
     var vm = this;
     window.getTotal  = vm.getTotal = ShopCart.getTotal.bind(ShopCart);
     window.getBooks  = vm.getBooks = ShopCart.getBooks.bind(ShopCart);
@@ -9,17 +9,33 @@
    
     vm.openLeftMenu = function() {
         $mdSidenav('left').toggle();
-      //vm.priceInCart--;
+   //  vm.priceInCart--;
    //  Invoice.refresh();
-    //  vm.priceInCart = ShopCart.getTotal();
-     
+   //  vm.priceInCart = ShopCart.getTotal();
     }
-    vm.testButton= function() {
-      
+    vm.checkout_popup = function(ev) {
+
+       $mdDialog.show({
+          controller: DialogTabController,
+          templateUrl: 'site-manager/tabDialog.tmpl.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutsideToClose:true
+        })
+        .then(function(answer) {
+          vm.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          vm.status = 'You cancelled the dialog.';
+        });
+
+        function DialogTabController(){
+
+        }
     }
 
+    
   }
-  function additemshopcart($mdSidenav,ShopCart){
+  function additemshopcart($mdSidenav,$mdDialog,ShopCart){
     var vm = this;
      vm.addmore = function(id){
        ShopCart.IncreasBookCount(id);
